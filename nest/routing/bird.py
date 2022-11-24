69a34625c5bb879c1ac188b60b336a63dead1e4c
B VIDEEP KUMAR REDDY <55957530+videep1@users.noreply.github.com>
2022-11-24 10:32:41 +0530

    Routing : different files for different framework

---
+++ nest/routing/bird.py
@@ -0,0 +1,185 @@
+import os
+import pwd
+import time
+import logging
+import importlib
+from os import mkdir, kill, path, listdir
+import atexit
+from shutil import rmtree, chown
+from signal import SIGTERM
+from typing import List
+from nest.exception import RequiredDependencyNotFound
+from nest.input_validator.input_validator import input_validator
+from nest.routing import routing_helper
+from nest.topology.id_generator import IdGen
+from nest.routing.static_routing import StaticRouting
+from nest.routing.zebra import Zebra
+from nest.routing.ldp import Ldp
+from nest.topology_map import TopologyMap
+from nest.user import User
+from nest import config
+from nest.topology import Node
+from nest.routing.routing_helper import RoutingHelper
+logger = logging.getLogger(__name__)
+
+class Bird(RoutingHelper):
+    def _create_directory(self, dir_path):
+        """
+        Creates a quagga/frr owned directory at `dir_path`
+
+        Parmeters
+        ---------
+        dir_path: path of the directory to be created
+
+        """
+        if path.exists(dir_path):
+            logger.warning("{dir_path} already exists")
+        else:
+            mkdir(dir_path)
+            if config.get_value("routing_suite") == "bird":
+                chown(dir_path, user=pwd.getpwuid(os.getuid())[0])
+            
+
+    def _create_conf_directory(self):
+        """
+        Creates a directory for holding routing related config
+        and pid files.
+        Override this to create directory at a location other than /tmp
+
+        Returns
+        -------
+        str:
+            path of the created directory
+        """
+        salt = config.get_value("routing_suite") + str(time.clock_gettime(0))
+        dir_path = f"/tmp/{salt}-configs_{IdGen.topology_id}"
+        self._create_directory(dir_path)
+        return dir_path
+
+    def _create_log_directory(self):
+        """
+        Creates a directory for holding routing log files.
+        Returns
+        -------
+        str:
+            path of the created directory
+        """
+        timestamp = time.strftime("%d-%m-%Y-%H:%M:%S")
+        log_path = f"{config.get_value('routing_suite')}-logs_{timestamp}"
+        self._create_directory(log_path)
+        return log_path
+
+    def _setup_default_routes(self):
+        """
+        Setup default routes in hosts
+        """
+        if config.get_value("routing_suite") == "bird":
+            for host in self.hosts:
+                host.add_route("DEFAULT", host.interfaces[0])
+            router_interfaces = set()
+            for router in self.routers:
+                for interface in router.interfaces:
+                    router_interfaces.add(interface)
+            for router in self.routers:
+                for interface in router.interfaces:
+                    if interface.pair not in router_interfaces:
+                        router.add_route(interface.pair.get_address(), interface)
+        
+
+    def _run_dyn_routing_bird(self):
+        """
+        to create config dir and run bird
+        """
+        logger.info("Running bird on routers")
+        self.socket_dir = ""
+        self.conf_dir = self._create_conf_directory()
+        if config.get_value("routing_logs"):
+            self.log_dir = self._create_log_directory()
+        for router in self.routers:
+            self._run_routing_protocol(router)
+        self._check_for_convergence()
+
+    
+
+    def _run_zebra(self, router):
+        """
+        Create required config file and run zebra
+        """
+        zebra = Zebra(
+            router.id,
+            self.ipv6_routing,
+            router.interfaces,
+            self.conf_dir,
+            log_dir=self.log_dir,
+        )
+        zebra.create_basic_config()
+        zebra.run()
+        self.zebra_list.append(zebra)
+
+    def _run_routing_protocol(self, router):
+        """
+        Create required config file and run `self.protocol`
+        """
+        if config.get_value("routing_suite") == "bird":
+            protocol = self.protocol_class(
+                router.id,
+                self.ipv6_routing,
+                router.interfaces,
+                self.conf_dir,
+                log_dir=self.log_dir,
+                socket_dir=self.socket_dir,
+            )
+        
+        protocol.create_basic_config()
+        protocol.run()
+        self.protocol_list.append(protocol)
+
+    def _run_ldp(self, router):
+        """
+        Create required config file and run ldp
+        """
+        mpls_interfaces = []
+        for interface in router.interfaces:
+            if interface.is_mpls_enabled():
+                mpls_interfaces.append(interface)
+        if len(mpls_interfaces) == 0:
+            raise Exception("MPLS isn't enabled in any interface!")
+        ldp = Ldp(
+            router.id,
+            self.ipv6_routing,
+            mpls_interfaces,
+            self.conf_dir,
+            log_dir=self.log_dir,
+        )
+        ldp.create_basic_config()
+        ldp.run()
+        self.ldp_list.append(ldp)
+
+    def _check_for_convergence(self):
+        """
+        Wait for the routing protocol to converge.
+        Override this for custom convergence check
+        """
+        logger.info("Waiting for %s to converge", self.protocol)
+        interval = 2
+        converged = False
+        # Ping between hosts until convergence
+        while not converged:
+            time.sleep(interval)
+            converged = True
+            for i in range(len(self.hosts)):
+                for j in range(i + 1, len(self.hosts)):
+                    for k_addr in (
+                        self.hosts[j]
+                        .interfaces[0]
+                        .get_address(not self.ipv6_routing, self.ipv6_routing, True)
+                    ):
+                        if not self.hosts[i].ping(k_addr.get_addr(), verbose=0):
+                            converged = False
+                            break
+                    if not converged:
+                        break
+                if not converged:
+                    break
+
+        logger.info("Routing completed")
--- nest/routing/routing_helper.py
+++ nest/routing/routing_helper.py
@@ -24,7 +24,9 @@ from nest.topology_map import TopologyMap
 from nest.user import User
 from nest import config
 from nest.topology import Node
-
+from nest.routing.frr import Frr
+from nest.routing.quagga import Quagga
+from nest.routing.bird import Bird
 logger = logging.getLogger(__name__)
 
 # pylint:disable=too-few-public-methods
@@ -147,7 +149,34 @@ class RoutingHelper:
             static_routing.run_static_routing()
         elif config.get_value("routing_suite") == "bird":
             try:
-                self._run_dyn_routing_bird()
+                bird= Bird(self.protocol,
+                        self.ipv6_routing,
+                        self.hosts,
+                        self.routers,
+                        self.ldp_routers
+                        )
+                bird._run_dyn_routing_bird()
+            except RequiredDependencyNotFound:
+                return
+        elif((self.protocol == "isis" or self.protocol == "ospf" or self.protocol == "rip")
+                    and config.get_value("routing_suite") == "quagga"):
+            try:
+                # Warning for using ISIS daemons along with other daemons
+                # since they interfere with other routing daemons (which may
+                # or may not be of ISIS)
+                
+                    
+                logger.warning(
+                        "ISIS or any protocol routing protocol in quagga might overwrite "
+                        "previously assigned routes by other RoutingHelpers"
+                    )
+                quagga= Quagga(self.protocol,
+                        self.ipv6_routing,
+                        self.hosts,
+                        self.routers,
+                        self.ldp_routers
+                        )
+                quagga._run_dyn_routing()
             except RequiredDependencyNotFound:
                 return
         else:
@@ -156,212 +185,26 @@ class RoutingHelper:
                 # since they interfere with other routing daemons (which may
                 # or may not be of ISIS)
                 if (
-                    self.protocol == "isis"
-                    and config.get_value("routing_suite") == "quagga"
+                    (self.protocol == "isis" or self.protocol == "ospf" or self.protocol == "rip")
+                    and config.get_value("routing_suite") == "frr"
                 ):
-                    logger.warning(
-                        "ISIS routing protocol in quagga might overwrite "
-                        "previously assigned routes by other RoutingHelpers"
-                    )
-                self._run_dyn_routing()
+                    #logger.warning(
+                    #    "ISIS routing protocol in quagga might overwrite "
+                    #    "previously assigned routes by other RoutingHelpers"
+                    #)
+                    frr= Frr(self.protocol,
+                        self.ipv6_routing,
+                        self.hosts,
+                        self.routers,
+                        self.ldp_routers
+                        )
+                frr._run_dyn_routing()
+                
             except RequiredDependencyNotFound:
                 return
 
-    def _create_directory(self, dir_path):
-        """
-        Creates a quagga/frr owned directory at `dir_path`
-
-        Parmeters
-        ---------
-        dir_path: path of the directory to be created
-
-        """
-        if path.exists(dir_path):
-            logger.warning("{dir_path} already exists")
-        else:
-            mkdir(dir_path)
-            if config.get_value("routing_suite") == "bird":
-                chown(dir_path, user=pwd.getpwuid(os.getuid())[0])
-            else:
-                chown(dir_path, user=config.get_value("routing_suite"))
-
-    def _create_conf_directory(self):
-        """
-        Creates a directory for holding routing related config
-        and pid files.
-        Override this to create directory at a location other than /tmp
-
-        Returns
-        -------
-        str:
-            path of the created directory
-        """
-        salt = config.get_value("routing_suite") + str(time.clock_gettime(0))
-        dir_path = f"/tmp/{salt}-configs_{IdGen.topology_id}"
-        self._create_directory(dir_path)
-        return dir_path
-
-    def _create_log_directory(self):
-        """
-        Creates a directory for holding routing log files.
-
-        Returns
-        -------
-        str:
-            path of the created directory
-        """
-        timestamp = time.strftime("%d-%m-%Y-%H:%M:%S")
-        log_path = f"{config.get_value('routing_suite')}-logs_{timestamp}"
-        self._create_directory(log_path)
-        return log_path
-
-    def _setup_default_routes(self):
-        """
-        Setup default routes in hosts
-        """
-        if config.get_value("routing_suite") == "bird":
-            for host in self.hosts:
-                host.add_route("DEFAULT", host.interfaces[0])
-            router_interfaces = set()
-            for router in self.routers:
-                for interface in router.interfaces:
-                    router_interfaces.add(interface)
-            for router in self.routers:
-                for interface in router.interfaces:
-                    if interface.pair not in router_interfaces:
-                        router.add_route(interface.pair.get_address(), interface)
-        else:
-            for host in self.hosts:
-                host.add_route(
-                    "DEFAULT",
-                    host.interfaces[0],
-                    host.interfaces[0].pair.get_address(
-                        not self.ipv6_routing, self.ipv6_routing, True
-                    )[0],
-                )
-
-    def _run_dyn_routing_bird(self):
-        """
-        to create config dir and run bird
-        """
-        logger.info("Running bird on routers")
-        self.socket_dir = ""
-        self.conf_dir = self._create_conf_directory()
-        if config.get_value("routing_logs"):
-            self.log_dir = self._create_log_directory()
-        for router in self.routers:
-            self._run_routing_protocol(router)
-        self._check_for_convergence()
-
-    def _run_dyn_routing(self):
-        """
-        Run zebra and `self.protocol`
-        """
-        if self.ipv6_routing:
-            logger.info("Running zebra and %s on routers (IPv6)", self.protocol)
-        else:
-            logger.info("Running zebra and %s on routers", self.protocol)
-        self.conf_dir = self._create_conf_directory()
-        if config.get_value("routing_logs"):
-            self.log_dir = self._create_log_directory()
-
-        for router in self.routers:
-            self._run_zebra(router)
-            self._run_routing_protocol(router)
-            if router in self.ldp_routers:
-                self._run_ldp(router)
-        self._check_for_convergence()
-
-    def _run_zebra(self, router):
-        """
-        Create required config file and run zebra
-        """
-        zebra = Zebra(
-            router.id,
-            self.ipv6_routing,
-            router.interfaces,
-            self.conf_dir,
-            log_dir=self.log_dir,
-        )
-        zebra.create_basic_config()
-        zebra.run()
-        self.zebra_list.append(zebra)
-
-    def _run_routing_protocol(self, router):
-        """
-        Create required config file and run `self.protocol`
-        """
-        if config.get_value("routing_suite") == "bird":
-            protocol = self.protocol_class(
-                router.id,
-                self.ipv6_routing,
-                router.interfaces,
-                self.conf_dir,
-                log_dir=self.log_dir,
-                socket_dir=self.socket_dir,
-            )
-        else:
-            protocol = self.protocol_class(
-                router.id,
-                self.ipv6_routing,
-                router.interfaces,
-                self.conf_dir,
-                log_dir=self.log_dir,
-            )
-        protocol.create_basic_config()
-        protocol.run()
-        self.protocol_list.append(protocol)
-
-    def _run_ldp(self, router):
-        """
-        Create required config file and run ldp
-        """
-        mpls_interfaces = []
-        for interface in router.interfaces:
-            if interface.is_mpls_enabled():
-                mpls_interfaces.append(interface)
-        if len(mpls_interfaces) == 0:
-            raise Exception("MPLS isn't enabled in any interface!")
-        ldp = Ldp(
-            router.id,
-            self.ipv6_routing,
-            mpls_interfaces,
-            self.conf_dir,
-            log_dir=self.log_dir,
-        )
-        ldp.create_basic_config()
-        ldp.run()
-        self.ldp_list.append(ldp)
-
-    def _check_for_convergence(self):
-        """
-        Wait for the routing protocol to converge.
-        Override this for custom convergence check
-        """
-        logger.info("Waiting for %s to converge", self.protocol)
-        interval = 2
-        converged = False
-        # Ping between hosts until convergence
-        while not converged:
-            time.sleep(interval)
-            converged = True
-            for i in range(len(self.hosts)):
-                for j in range(i + 1, len(self.hosts)):
-                    for k_addr in (
-                        self.hosts[j]
-                        .interfaces[0]
-                        .get_address(not self.ipv6_routing, self.ipv6_routing, True)
-                    ):
-                        if not self.hosts[i].ping(k_addr.get_addr(), verbose=0):
-                            converged = False
-                            break
-                    if not converged:
-                        break
-                if not converged:
-                    break
-
-        logger.info("Routing completed")
-
+    
+#
     def _is_node_list(self, arg_name, node_list):
         """
         Checks if `node_list` is a list of Nodes.
@@ -413,8 +256,8 @@ class RoutingHelper:
                         pass
 
         # Delete config directory
-        if self.conf_dir is not None and path.isdir(self.conf_dir):
-            rmtree(self.conf_dir)
+        if path.isdir(self.conf_dir):
+            rmtree(self.conf_dir, ignore_errors=True)
 
         # Change ownership of log files to current user
         if self.log_dir is not None and path.isdir(self.log_dir):
